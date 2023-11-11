ce7048170a4edc69f53d4075304f2f5c407c3861
Jose Carrera <a3520110261@alumno.uttehuacan.edu.mx>
2023-11-11 14:18:50 -0600

    commit
    To Job in Ionic

--- lib/index.dart
+++
@@ -1,6 +0,0 @@
-// Export pages
-export '/splash_screen/splash_screen_widget.dart' show SplashScreenWidget;
-export '/crear_cuenta/crear_cuenta_widget.dart' show CrearCuentaWidget;
-export '/home/home_widget.dart' show HomeWidget;
-export '/profile/profile_widget.dart' show ProfileWidget;
-export '/search/search_widget.dart' show SearchWidget;
