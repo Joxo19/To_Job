9abbf0b930852b659a4ddfd21bef9263c0af1bb9
Jose Carrera <a3520110261@alumno.uttehuacan.edu.mx>
2023-11-12 22:32:01 -0600

    commit 0.3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCjf2a8zIFcRKFUYC2_ofN9C9ogEVNYvVo",
+    authDomain: "tojob-97dbc.firebaseapp.com",
+    projectId: "tojob-97dbc",
+    storageBucket: "tojob-97dbc.appspot.com",
+    messagingSenderId: "724852094912",
+    appId: "1:724852094912:web:42abc03e681c34bd667a25"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCjf2a8zIFcRKFUYC2_ofN9C9ogEVNYvVo",
+    authDomain: "tojob-97dbc.firebaseapp.com",
+    projectId: "tojob-97dbc",
+    storageBucket: "tojob-97dbc.appspot.com",
+    messagingSenderId: "724852094912",
+    appId: "1:724852094912:web:42abc03e681c34bd667a25"
+  }
 };
 
 /*
